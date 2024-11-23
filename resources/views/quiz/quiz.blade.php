0f9935caaab584c798cd130e4a218e696be32aa8
revazra <nabilazrarevaa@gmail.com>
2024-11-23 21:28:15 +0700

    ubah title

---
+++ resources/views/quiz/quiz.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kuis</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="quiz.css">
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="SmartEdu.png" height="40"/>
+        </div>
+        <div class="right-menu">
+            <div class="search-bar">
+                <input placeholder="Cari.." type="text"/>
+                <i class="fa fas-search">
+                </i>
+            </div>
+            <div class="user">
+            <img src="https://storage.googleapis.com/a1aa/image/AbjGzYRcDex2KqcQJT3Pq51pDrfU1v2vxYAWEQWtaQ5ZSkzTA.jpg" width="40"/>
+            <span>
+                Halo, Nabigha
+            </span>
+            </div>
+        </div>
+    </div>
+    <div class="main">
+        <div class="content">
+            <div class="quiz-header">
+                <div class="title">Machine Learning</div>
+                <div class="timer">
+                    <span>Waktu tersisa</span>
+                    <div class="time-box">00:00</div>
+                </div>
+            </div>
+            <div class="quiz">
+                <div class="question">
+                    <p>1. Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <ul class="options">
+                        <li>A. Jawaban 1</li>
+                        <li>B. Jawaban 2</li>
+                        <li>C. Jawaban 3</li>
+                        <li>D. Jawaban 4</li>
+                        <li>E. Jawaban 5</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="question">
+                <p>2. Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <ul class="options">
+                    <li>A. Jawaban 1</li>
+                    <li>B. Jawaban 2</li>
+                    <li>C. Jawaban 3</li>
+                    <li>D. Jawaban 4</li>
+                    <li>E. Jawaban 5</li>
+                </ul>
+            </div>
+            <div class="navigation">
+                <button>
+                    <i class="fas fa-arrow-left"></i>
+                    Previous
+                </button>
+                <button>
+                    <i class="fas fa-arrow-right"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
